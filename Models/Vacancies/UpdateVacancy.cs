2481643541ebce04c288b22dbc6c191173bf0226
Rikutou <alex.evseeva@gmail.com>
2024-05-26 21:38:20 +0300

    Fixed vacancy.Payment issue

--- Models/Vacancies/UpdateVacancy.cs
+++ Models/Vacancies/UpdateVacancy.cs
@@ -8,7 +8,7 @@ public class UpdateVacancy
     [Required] public string FirmName { get; set; }
     [Required] public int ProfessionId { get; set; }
     public string? WorkingCondition { get; set; }
-    [Range(0, 120_000_000)] public decimal Payment { get; set; }
+    [Range(0, 120_000_000)] public decimal? Payment { get; set; }
     public string? LivingCondition { get; set; }
     [Required] public string Requirements { get; set; }
 }
