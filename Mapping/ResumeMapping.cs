e65b801893b7542eeb3ad1217fc6eb0912158788
Rikutou <alex.evseeva@gmail.com>
2024-05-26 21:23:41 +0300

    Add create new and edit resume

---
+++ Mapping/ResumeMapping.cs
@@ -0,0 +1,39 @@
+﻿using LaborExchange.Frontend.Models.Resume;
+
+namespace LaborExchange.Frontend;
+
+public static class ResumeMapping
+{
+    public static CreateResume toCreateDto(this Resume resume) =>
+        new CreateResume{
+            FirstName = resume.FirstName,
+            LastName = resume.LastName,
+            ProfessionId = resume.Profession.Id,
+            Education = resume.Education,
+            LastPositionFirm = resume.LastPositionFirm,
+            LastPositionName = resume.LastPositionName,
+            ReasonOfDismissal = resume.ReasonOfDismissal,
+            MartialStatus = resume.MartialStatus,
+            LivingCondition = resume.LivingCondition,
+            Email = resume.Email,
+            Phone = resume.Phone,
+            Requirements = resume.Requirements
+        };
+
+    public static UpdateResume toUpdateDto(this CreateResume resume, int id) =>
+        new UpdateResume{
+            Id = id,
+            FirstName = resume.FirstName,
+            LastName = resume.LastName,
+            ProfessionId = resume.ProfessionId,
+            Education = resume.Education,
+            LastPositionFirm = resume.LastPositionFirm,
+            LastPositionName = resume.LastPositionName,
+            ReasonOfDismissal = resume.ReasonOfDismissal,
+            MartialStatus = resume.MartialStatus,
+            LivingCondition = resume.LivingCondition,
+            Email = resume.Email,
+            Phone = resume.Phone,
+            Requirements = resume.Requirements
+        };
+}
